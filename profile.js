2da0b34d2934232c1e40992d9c6b17e420138e90
charlerae <contactcharlerae@gmail.com>
2020-04-14 14:26:58 -0400

    Initial commit

---
+++ profile.js
@@ -0,0 +1,51 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    // create header containing name
+    const name = document.createElement('h1');
+    name.setAttribute('id', 'name');
+    const nameText = document.createTextNode('Charle Rae');
+    name.appendChild(nameText);
+    document.body.appendChild(name);
+
+
+    // create unorderd list of 4+ details about myself
+    const firstList = document.createElement('ul');
+    firstList.setAttribute('id', 'firstList');
+
+        // list items
+    const oneItem = document.createElement('li');
+    const twoItem = document.createElement('li');
+    const threeItem = document.createElement('li');
+    const fourItem = document.createElement('li');
+
+        // list item text content
+    const oneContent = document.createTextNode("Crochet");
+    const twoContent = document.createTextNode("Drawing");
+    const threeContent = document.createTextNode("Sewing");
+    const fourContent = document.createTextNode("Macrame");
+
+        // append list content to list items
+
+
+
+    // refactor to make list programmatic
+
+
+    // add css classes and styles
+
+
+    // add a clock with date object
+
+
+    // add more sections to page like an image
+
+
+    // use other element methods
+
+
+    // fire javascript on different DOM events
+
+
+    // make a countdown clock
+
+})
